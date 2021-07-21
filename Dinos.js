c64da8ad110b5e92a6e8e414db036fa3cb43e60d
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-21 09:59:27 +0300

    Create Dinos.js

---
+++ Dinos.js
@@ -0,0 +1,18 @@
+// __Author__ __Lencof__
+// Dinos.js
+
+var Dinos = [];
+Dinos[0] = "Tyrannosaurus";
+Dinos[1] = "Velociraptor";
+Dinos[2] = "Stegosaurus";
+Dinos[3] = "Triceratops";
+Dinos[4] = "Brachiosaurus";
+Dinos[5] = "Pteranodon";
+Dinos[6] = "Apatosaurus";
+Dinos[7] = "Diplodocus";
+Dinos[8] = "Compsognath";
+
+Dinos;
+["Tyrannosaurus", "Velociraptor", "Stegosaurus", "Triceratops", 
+"Brachiosaurus", "Pteranodon", "Apatosaurus", "Diplodocus", 
+"Compsognath"]
