cd41b48e43f527dd802e962b445a7f3ee1269963
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-31 06:16:54 +0300

    Update friends.js

--- friends.js
+++ friends.js
@@ -7,3 +7,11 @@ var kate = { name: "kate", age: 9, luckyNumbers: [1, 2, 3] };
 
 var friends = [anna, dave, kate];
 
+friends[0];
+// { name: "anna", age: 11, luckyNumbers: [2, 4, 8, 16] };
+
+friends[1];
+// { name: "dave", age: 5, luckyNumbers: [3, 9, 40] };
+
+friends[2];
+// { name: "kate", age: 9, luckyNumbers: [1, 2, 3] };
