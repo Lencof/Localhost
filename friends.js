be4523434c2341625de40cf9a69e2b2b32c71330
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-31 06:14:58 +0300

    Create friends.js

---
+++ friends.js
@@ -0,0 +1,9 @@
+// __Author__ __Lencof__
+// friends.js
+
+var anna = { name: "anna", age: 11, luckyNumbers: [2, 4, 8, 16] };
+var dave = { name: "dave", age: 5, luckyNumbers: [3, 9, 40] };
+var kate = { name: "kate", age: 9, luckyNumbers: [1, 2, 3] };
+
+var friends = [anna, dave, kate];
+
