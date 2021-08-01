f50144f8b7043e0e21444e63eadd662a0bd52534
Lencof <71154457+Lencof@users.noreply.github.com>
2021-08-01 11:57:14 +0300

    Update Movies.js

--- Movies.js
+++ Movies.js
@@ -1,1 +1,24 @@
-// __
+// __Author__ __Lencof__
+// Movies.js
+
+var movies = {
+  "Finding Nemo": {
+  releaseDate: 2003,
+  duration: 100,
+  actors: ["Albert Brooks", "Ellen DeGeneres", 
+  "Alexander Gould"],
+  format: "DVD"
+  },
+  "Star Wars Episode VI: Return of the Jedi": {
+  releaseDate: 1983,
+  duration: 134,
+  actors: ["Mark Hamill", "Harrison Ford", "Carrie Fisher"],
+  format: "DVD"
+  },
+  "Harry Potter and the Goblet of Fire": {
+  releaseDate: 2005,
+  duration: 157,
+  actors: ["Daniel Radcliffe", "Emma Watson", "Rupert Grint"],
+  format: "Blu-ray"
+  }
+};
