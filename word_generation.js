fbae30fa7c45075412735f750a9880f679bd8280
Lencof <71154457+Lencof@users.noreply.github.com>
2021-08-18 08:41:58 +0300

    Update word_generation.js

--- word_generation.js
+++ word_generation.js
@@ -1,2 +1,6 @@
 // __Author__ __Lencof__
 // word_generation.js
+
+var randomBodyParts = ["eye", "nose", "scull"];
+var randomAdjectives = ["smelly", "dull", "stupid"];
+var randomWords = ["fly", "otter", "дубина", "monkey", "rat"];
