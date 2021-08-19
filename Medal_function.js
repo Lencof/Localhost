58dc01dff79170ee1f27b625f8ce10bbad1a3848
Lencof <71154457+Lencof@users.noreply.github.com>
2021-08-19 09:47:16 +0300

    Update Medal_function.js

--- Medal_function.js
+++ Medal_function.js
@@ -1,2 +1,12 @@
 // __Author__ __Lencof__
 // Medal_function.js
+
+var medalForScore = function (score) {
+  if (score < 3) {
+     return "Bronze ";
+  }
+  if (score < 7) {
+     return "Silver";
+ }
+     return "Gold";
+};
