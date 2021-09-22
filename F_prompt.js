33121307d7febaeebb90de559ed292bcbeb8f307
Lencof <71154457+Lencof@users.noreply.github.com>
2021-09-22 08:57:11 +0300

    Update F_prompt.js

--- F_prompt.js
+++ F_prompt.js
@@ -2,3 +2,6 @@ let userName = prompt('What your name?');
 let userAge = prompt('How old are you??');
 let userHobby = prompt('Tell us about a hobby you like!');
 
+alert(`What your name: ${userName}` );
+alert(`You are full years old.: ${userAge}`);
+alert(`You have an interesting hobby: ${userHobby}`);
